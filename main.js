8603017176d1282a1c7b1742eb16c933ec9475c6
dashzero203 <77020857+dashzero203@users.noreply.github.com>
2023-06-10 22:35:49 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -2,12 +2,30 @@
 var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 var browserHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
+// Check if the device is in portrait mode (height > width)
+var isPortrait = browserHeight > browserWidth;
+
+// Calculate the desired width and height based on the aspect ratio
+var desiredWidth = isPortrait ? browserHeight * (1080 / 600) : browserWidth;
+var desiredHeight = isPortrait ? browserHeight : browserWidth * (600 / 1080);
+
+desiredHeight *= 0.85;
+desiredWidth *= 0.9;
+
 const config = {
   type: Phaser.AUTO,
-  width: browserWidth,
-  height: browserHeight,
-  scene: [Menu, Play, Setting],
+  width: desiredWidth,
+  height: desiredHeight,
+  physics: {
+    default: 'arcade',
+    arcade: {
+        debug: false
+    }
+},
+  scene: [Menu, Play, Credit],
+  scale: {autoCenter: Phaser.Scale.CENTER_BOTH}
 };
 
 // Create the Phaser game instance
 const game = new Phaser.Game(config);
+
