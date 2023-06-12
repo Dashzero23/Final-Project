5b8d1f606b51c7fb586b262eb33d6b27953c83fa
dashzero203 <77020857+dashzero203@users.noreply.github.com>
2023-06-12 14:16:41 -0700

    use 2 different pointers for shoot and move

--- scenes.js
+++ scenes.js
@@ -283,52 +283,63 @@ class Play extends Phaser.Scene {
         }
 
         let controlRect = new Phaser.Geom.Rectangle(40 * this.scalex, 440 * this.scaley, 210 * this.scalex, 220 * this.scaley);
-        const shootPointer = this.input.addPointer();
-
-        this.input.on('pointerdown', (shootPointer) => {
-            const currentTime = this.time.now;
-            const isPointerInControlArea = controlRect.contains(shootPointer.x, shootPointer.y);
-
-            if (isPointerInControlArea) {
-                return;
+        let shootPointer;
+        
+        this.input.on('pointerdown', (pointer) => {
+          const currentTime = this.time.now;
+          const isPointerInControlArea = controlRect.contains(pointer.x, pointer.y);
+        
+          if (isPointerInControlArea) {
+            return;
+          }
+        
+          if (!shootPointer && currentTime - this.lastCardTime >= this.cardDelay) {
+            this.lastCardTime = currentTime;
+        
+            const playerX = this.player.x;
+            const playerY = this.player.y;
+            const pointerX = pointer.worldX;
+            const pointerY = pointer.worldY;
+        
+            // Calculate the angle between the player and the pointer
+            const angle = Phaser.Math.Angle.Between(playerX, playerY, pointerX, pointerY);
+        
+            // Create the three cards with different angles
+            for (let i = -1; i <= 1; i++) {
+              const card = this.physics.add.sprite(playerX, playerY, 'cards');
+              card.setScale(desiredWidth / background.width, desiredHeight / background.height);
+              const cardAngle = angle + Phaser.Math.DegToRad(i * 15);
+              const cardSpeed = 500;
+              this.cards.add(card);
+              // Set the velocity of the card towards the pointer
+              card.setVelocity(
+                Math.cos(cardAngle) * cardSpeed,
+                Math.sin(cardAngle) * cardSpeed
+              );
+        
+              card.setRotation(cardAngle);
+        
+              this.time.delayedCall(3000, () => {
+                card.destroy();
+              }, [], this);
             }
-            
-            if (currentTime - this.lastCardTime >= this.cardDelay) {
-                this.lastCardTime = currentTime;
-
-                const playerX = this.player.x;
-                const playerY = this.player.y;
-                const pointerX = shootPointer.worldX;
-                const pointerY = shootPointer.worldY;
-            
-                // Calculate the angle between the player and the pointer
-                const angle = Phaser.Math.Angle.Between(playerX, playerY, pointerX, pointerY);
-            
-                // Create the three cards with different angles
-                for (let i = -1; i <= 1; i++) {
-                    const card = this.physics.add.sprite(playerX, playerY, 'cards');
-                    card.setScale(desiredWidth / background.width, desiredHeight / background.height);
-                    const cardAngle = angle + Phaser.Math.DegToRad(i * 15);
-                    const cardSpeed = 500;
-                    this.cards.add(card);
-                    // Set the velocity of the card towards the pointer
-                    card.setVelocity(
-                        Math.cos(cardAngle) * cardSpeed,
-                        Math.sin(cardAngle) * cardSpeed
-                    );
-
-                    card.setRotation(cardAngle);
-
-                    this.time.delayedCall(3000, () => {
-                        card.destroy();
-                    }, [], this);
-
-                    
-                }
-            };
-
-            this.physics.add.collider(this.cards, this.enemies, this.Cardhit, null, this);
+          }
+        
+          this.physics.add.collider(this.cards, this.enemies, this.Cardhit, null, this);
         });
+        
+        this.input.on('pointerup', (pointer) => {
+          if (pointer === shootPointer) {
+            shootPointer = null;
+          }
+        });
+        
+        this.input.on('pointerdown', (pointer) => {
+          if (!shootPointer && !controlRect.contains(pointer.x, pointer.y)) {
+            shootPointer = pointer;
+          }
+        });
+        
     }
 
     spawnEnemy() {
